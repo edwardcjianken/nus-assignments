ad08f58f8881d2fd67ddce0364fa224ef426867b
Edward Chia <edwardc.jianken@gmail.com>
2025-05-14 01:00:57 +0800

    added js code for assignment-week-5

---
+++ task1.js
@@ -0,0 +1,7 @@
+const name = "Edward Chia";
+const age = 28;
+const isStudent = true;
+
+console.log("Name:", name, "| Data Type:", typeof name);
+console.log("Age:", age, "| Data Type:", typeof age);
+console.log("Is Student:", isStudent, "| Data Type:", typeof isStudent);
---
+++ task3.js
@@ -0,0 +1,5 @@
+const fruits = ["Watermelon", "Lemon", "Apple", "Orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
